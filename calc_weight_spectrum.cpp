7a08480c4497ff21b7172227d6b96d34cd83467b
Smolskiy Maxim <mithridatus@mail.ru>
2020-07-13 22:23:31 +0300

    Улучшена программа

--- calc_weight_spectrum.cpp
+++ calc_weight_spectrum.cpp
@@ -2,102 +2,83 @@
 #include <valarray>
 #include <vector>
 #include <thread>
-#include <iostream>
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
-using ull_t = unsigned long long;
+using ull_type = unsigned long long;
 
-static size_t constexpr SIZE_BL = sizeof(uintptr_t) << 3;
-using bl_t = bitset<SIZE_BL>;
-using vec_t = valarray<bl_t>;
-using set_vec_t = vector<vec_t>;
+static size_t constexpr PROB_SIZE_REG_BIT = sizeof(uintptr_t) * CHAR_BIT;
 
-using weig_spec_t = valarray<ull_t>;
-using set_weig_spec_t = vector<weig_spec_t>;
+using bl_type = bitset<PROB_SIZE_REG_BIT>;
+using bit_bl_type = bl_type::reference;
 
-using set_thr_t = vector<thread>;
+using vec_type = valarray<bl_type>;
+using set_vec_type = vector<vec_type>;
+
+using weig_spec_type = valarray<ull_type>;
+using set_weig_spec_type = vector<weig_spec_type>;
+
+using set_thr_type = vector<thread>;
 
 static size_t const LOG_2_DEF_NUM_THR = 3;
 
-void report_error(string const &mes) {
-    cerr << mes << ".\n";
-    exit(EXIT_FAILURE);
+// Вычисляет количество блоков для хранения вектора.
+size_t calc_num_bl(size_t N) {
+    return (N + PROB_SIZE_REG_BIT - 1) / PROB_SIZE_REG_BIT;
 }
 
-set_vec_t calc_set_bas_vec(string const &name_file, size_t &K, size_t &N)
-{
-    ifstream inp(name_file);
+// Возвращает i-ый бит вектора.
+bool i_bit(const vec_type &vec, size_t i) {
+    return vec[i / PROB_SIZE_REG_BIT][i % PROB_SIZE_REG_BIT];
+}
 
-    if (!inp.is_open()) {
-        report_error("Can't open input file \"" + name_file + '\"');
+// Обеспечивает доступ к i-ому биту вектора.
+bit_bl_type acc_i_bit(vec_type &vec, size_t i) {
+    return vec[i / PROB_SIZE_REG_BIT][i % PROB_SIZE_REG_BIT];
+}
+
+// Проверяет входную строку на наличие ошибок.
+void check_line(string const &line, size_t N, string const &name_inp_file) {
+    if (line.length() != N) {
+        throw exception(("In input file \"" + name_inp_file + "\" strings have different lengths").c_str());
     }
 
-    set_vec_t mat_bas_vec;
-    size_t num_bas_vec = 0;
-    string line;
-    bool is_first_line = true;
-    while (getline(inp, line)) {
-        if (!is_first_line && line.length() != N) {
-            report_error("In input file \"" + name_file + "\" strings have different lengths");
-        }
+    if (line.find_first_not_of("01\r\n") != string::npos) {
+        throw exception(("In input file \"" + name_inp_file + "\" string \"" + line + "\" has unallowed character").c_str());
+    }
+}
 
-        if (line.find_first_not_of("01\r\n") != string::npos) {
-            report_error("In input file \"" + name_file + "\" string \"" + line + "\" has unallowed character");
-        }
+// Конвертирует строку в вектор.
+vec_type conv_line_vec(string const &line) {
+    vec_type vec(calc_num_bl(line.size()));
+    for (size_t ind_ch = 0; ind_ch < line.size(); ++ind_ch) {
+        acc_i_bit(vec, ind_ch) = line[ind_ch] == '1';
+    }
+    return vec;
+}
 
-        N = line.length();
-        is_first_line = false;
-        K++;
-
-        vec_t vec((N + SIZE_BL - 1) / SIZE_BL);
-        for (size_t i = 0; i < N; i++) {
-            vec[i / SIZE_BL][i % SIZE_BL] = line[i] == '1';
-        }
-
-        mat_bas_vec.resize(N);
-        for (size_t i = 0; i < N; i++) {
-            if (vec[i / SIZE_BL][i % SIZE_BL]) {
-                if (mat_bas_vec[i].size()) {
-                    vec ^= mat_bas_vec[i];
-                } else {
-                    mat_bas_vec[i] = vec;
-                    num_bas_vec++;
-                    break;
-                }
+// Добавляет вектор в матрицу базисных (линейно независимых) векторов, если он является базисным - реализует прямой ход аналога метода Гаусса.
+bool add_vec_mat_bas_vec(vec_type &vec, set_vec_type &mat_bas_vec, size_t N) {
+    for (size_t ind_bit = 0; ind_bit < N; ++ind_bit) {
+        if (i_bit(vec, ind_bit)) {
+            if (mat_bas_vec[ind_bit].size()) {
+                vec ^= mat_bas_vec[ind_bit];
+            }
+            else {
+                mat_bas_vec[ind_bit] = vec;
+                return true;
             }
         }
     }
-
-    if (!N) {
-        report_error("Input file \"" + name_file + "\" is empty");
-    }
-
-    set_vec_t set_bas_vec(num_bas_vec);
-    size_t i = 0;
-    for (auto const &vec : mat_bas_vec) {
-        if (vec.size()) {
-            set_bas_vec[i] = vec;
-            i++;
-        }
-    }
-
-    return set_bas_vec;
+    return false;
 }
 
-weig_spec_t calc_full_bas_weig_spec(size_t N) {
-    weig_spec_t full_bas_weig_spec(N + 1);
-    full_bas_weig_spec[0] = 1;
-    for (size_t i = 1; i <= N; i++) {
-        full_bas_weig_spec[i] = full_bas_weig_spec[i - 1] * (N - i + 1) / i;
-    }
-    return full_bas_weig_spec;
-}
-
-size_t calc_weig_vec(vec_t const &vec) {
+// Вычисляет вес вектора.
+size_t calc_weig_vec(vec_type const &vec) {
     size_t weig_vec = 0;
     for (auto const &bl : vec) {
         weig_vec += bl.count();
@@ -105,81 +86,173 @@ size_t calc_weig_vec(vec_t const &vec) {
     return weig_vec;
 }
 
-size_t calc_ind_bas_vec(ull_t comb) {
-    comb ^= comb + 1;
-    size_t ind_bas_vec = 0;
-    while (comb) {
-        comb &= comb - 1;
-        ind_bas_vec++;
+// Вычисляет множество из B <= min { K, N } базисных (линейно независимых) векторов.
+//         Построчно считывает входной файл.
+//         Проверяет строку на наличие ошибок.
+//         Конвертирует строку в вектор.
+//         Добавляет вектор в матрицу базисных (линейно независимых) векторов, если он является базисным - реализует прямой ход аналога метода Гаусса.
+//     Конвертирует матрицу базисных (линейно независимых) векторов в множество базисных векторов.
+//     Исключает нулевые столбцы.
+set_vec_type calc_set_bas_vec(string const &name_inp_file, size_t &N, size_t &K) {
+    ifstream inp_file(name_inp_file);
+
+    if (!inp_file.is_open()) {
+        throw exception(("Can't open input file \"" + name_inp_file + '\"').c_str());
     }
-    return ind_bas_vec - 1;
+
+    string line;
+    getline(inp_file, line);
+    K = 1;
+    N = line.length();
+    check_line(line, N, name_inp_file);
+    
+    vec_type vec = conv_line_vec(line);
+    set_vec_type mat_bas_vec(N);
+    size_t num_bas_vec = add_vec_mat_bas_vec(vec, mat_bas_vec, N);
+
+    while (getline(inp_file, line)) {
+        ++K;
+        check_line(line, N, name_inp_file);
+
+        vec = conv_line_vec(line);
+        num_bas_vec += add_vec_mat_bas_vec(vec, mat_bas_vec, N);
+    }
+
+    if (!N) {
+        throw exception(("Input file \"" + name_inp_file + "\" is empty").c_str());
+    }
+
+    set_vec_type set_bas_vec;
+    set_bas_vec.reserve(num_bas_vec);
+    for (auto const &bas_vec : mat_bas_vec) {
+        if (bas_vec.size()) {
+            set_bas_vec.push_back(bas_vec);
+        }
+    }
+
+    vec_type or_bas_vec(set_bas_vec[0].size());
+    for (auto const &bas_vec : set_bas_vec) {
+        or_bas_vec |= bas_vec;
+    }
+
+    size_t num_col = N;
+    N = calc_weig_vec(or_bas_vec);
+    set_vec_type set_bas_vec_nonz_col(set_bas_vec.size(), vec_type(calc_num_bl(N)));
+    size_t ind_nonz_col = 0;
+    for (size_t ind_col = 0; ind_col < num_col; ++ind_col) {
+        if (i_bit(or_bas_vec, ind_col)) {
+            for (size_t ind_bas_vec = 0; ind_bas_vec < set_bas_vec.size(); ++ind_bas_vec) {
+                acc_i_bit(set_bas_vec_nonz_col[ind_bas_vec], ind_nonz_col) = i_bit(set_bas_vec[ind_bas_vec], ind_col);
+            }
+            ++ind_nonz_col;
+        }
+    }
+
+    return set_bas_vec_nonz_col;
 }
 
-void calc_part_bas_weig_spec(size_t i, set_vec_t const &set_bas_vec, weig_spec_t &part_bas_weig_spec) {
-    vec_t vec(set_bas_vec[0].size());
-    size_t log_2_num_thr = min(LOG_2_DEF_NUM_THR, set_bas_vec.size());
-    ull_t init_comb = i * (1ULL << (set_bas_vec.size() - log_2_num_thr));
-    ull_t init_code_Gray = init_comb ^ (init_comb >> 1);
-    size_t j = 0;
+// Вычисляет полный базисный весовой спектр - { C(N, i) | i = 0..N }, где C(N, i) - биномиальный коэффициент из N по i.
+weig_spec_type calc_full_bas_weig_spec(size_t N) {
+    weig_spec_type C_N(N + 1);
+    C_N[0] = 1;
+    for (size_t i = 1; i <= N; ++i) {
+        C_N[i] = C_N[i - 1] * (N - i + 1) / i;
+    }
+    return C_N;
+}
+
+// Вычисляет индекс меняющегося бита в (i + 1)-ом коде Грея - количество единичных битов в числе (i xor (i + 1)) без единицы.
+size_t calc_ind_ch_bit_next_code_Gray(ull_type ind_code_Gray) {
+    ull_type num = ind_code_Gray ^ (ind_code_Gray + 1);
+    size_t num_1_bit_num = 0;
+    while (num) {
+        num &= num - 1;
+        ++num_1_bit_num;
+    }
+    return num_1_bit_num - 1;
+}
+
+// Вычисляет частичный базисный весовой спектр, используя код Грея.
+void calc_part_bas_weig_spec(size_t ind_part, set_vec_type const &set_bas_vec, weig_spec_type &part_bas_weig_spec) {
+    size_t const log_2_num_thr = min(LOG_2_DEF_NUM_THR, set_bas_vec.size());
+    ull_type const num_ind_code_Gray_part = 1ULL << (set_bas_vec.size() - log_2_num_thr);
+    ull_type const init_ind_code_Gray = ind_part * num_ind_code_Gray_part;
+    ull_type init_code_Gray = init_ind_code_Gray ^ (init_ind_code_Gray >> 1);
+    vec_type vec(set_bas_vec[0].size());
+    size_t ind_bit_init_code_Gray = 0;
     while (init_code_Gray) {
         if (init_code_Gray & 1) {
-            vec ^= set_bas_vec[j];
+            vec ^= set_bas_vec[ind_bit_init_code_Gray];
         }
-        j++;
+        ++ind_bit_init_code_Gray;
         init_code_Gray >>= 1;
     }
-    part_bas_weig_spec[calc_weig_vec(vec)]++;
+    ++part_bas_weig_spec[calc_weig_vec(vec)];
 
-    for (ull_t comb = init_comb; comb < init_comb + (1ULL << (set_bas_vec.size() - log_2_num_thr)) - 1; comb++) {
-        vec ^= set_bas_vec[calc_ind_bas_vec(comb)];
-        part_bas_weig_spec[calc_weig_vec(vec)]++;
+    for (ull_type ind_code_Gray = init_ind_code_Gray; ind_code_Gray < init_ind_code_Gray + num_ind_code_Gray_part - 1; ++ind_code_Gray) {
+        vec ^= set_bas_vec[calc_ind_ch_bit_next_code_Gray(ind_code_Gray)];
+        ++part_bas_weig_spec[calc_weig_vec(vec)];
     }
 }
 
-weig_spec_t calc_weig_spec(set_vec_t const &set_bas_vec, size_t K, size_t N) {
-    weig_spec_t bas_weig_spec(N + 1);
-
+// Вычисляет базисный весовой спектр.
+// Если B == N, то вычисляет полный базисный весовой спектр.
+// Если B != N, то:
+//     по возможности, параллельно вычисляет частичные базисные весовые спектры;
+//     вычисляет базисный весовой спектр - сумму частичных базисных весовых спектров.
+weig_spec_type calc_bas_weig_spec(set_vec_type const &set_bas_vec, size_t N) {
     if (set_bas_vec.size() == N) {
-        bas_weig_spec = calc_full_bas_weig_spec(N);
-    } else {
-        size_t log_2_num_thr = min(LOG_2_DEF_NUM_THR, set_bas_vec.size());
-        size_t num_thr = size_t(1) << log_2_num_thr;
-        set_thr_t set_thr(num_thr);
-        set_weig_spec_t set_part_bas_weig_spec(num_thr, weig_spec_t(N + 1));
-        for (size_t i = 0; i < num_thr; i++) {
-            set_thr[i] = thread(calc_part_bas_weig_spec, i, cref(set_bas_vec), ref(set_part_bas_weig_spec[i]));
-        }
-
-        for (size_t i = 0; i < num_thr; i++) {
-            set_thr[i].join();
-            bas_weig_spec += set_part_bas_weig_spec[i];
-        }
+        return calc_full_bas_weig_spec(N);
     }
 
-    return bas_weig_spec * (1ULL << (K - set_bas_vec.size()));
-}
-
-void write_weig_spec(string const &name_file, weig_spec_t const &weig_spec) {
-    ofstream out(name_file);
-
-    if (!out.is_open()) {
-        report_error("Can't open output file \"" + name_file + '\"');
+    size_t const log_2_num_thr = min(LOG_2_DEF_NUM_THR, set_bas_vec.size());
+    size_t const num_thr = size_t(1) << log_2_num_thr;
+    set_thr_type set_thr;
+    set_thr.reserve(num_thr);
+    set_weig_spec_type set_part_bas_weig_spec(num_thr, weig_spec_type(N + 1));
+    for (size_t ind_part = 0; ind_part < num_thr; ++ind_part) {
+        set_thr.emplace_back(calc_part_bas_weig_spec, ind_part, cref(set_bas_vec), ref(set_part_bas_weig_spec[ind_part]));
     }
 
-    for (size_t i = 0; i < weig_spec.size(); i++)
-        out << i << '\t' << weig_spec[i] << '\n';
+    weig_spec_type bas_weig_spec(N + 1);
+    for (size_t ind_part = 0; ind_part < num_thr; ++ind_part) {
+        set_thr[ind_part].join();
+        bas_weig_spec += set_part_bas_weig_spec[ind_part];
+    }
+    return bas_weig_spec;
 }
 
+// Вычисляет весовой спектр - произведение базисного весового спектра и 2 ^ (K - B).
+weig_spec_type calc_weig_spec(set_vec_type const &set_bas_vec, size_t N, size_t K) {
+    return calc_bas_weig_spec(set_bas_vec, N) * (1ULL << (K - set_bas_vec.size()));
+}
+
+// Записывает весовой спектр в выходной файл.
+void write_weig_spec(string const &name_out_file, weig_spec_type const &weig_spec) {
+    ofstream out_file(name_out_file);
+
+    if (!out_file.is_open()) {
+        throw exception(("Can't open output file \"" + name_out_file + '\"').c_str());
+    }
+
+    for (size_t weig = 0; weig < weig_spec.size(); ++weig)
+        out_file << weig << '\t' << weig_spec[weig] << '\n';
+}
+
+// Главная функция, аргументы - имена входного и выходного файлов
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        report_error("Wrong number of arguments - expected 3, found " + to_string(argc));
+    try {
+        if (argc != 3) {
+            throw exception(("Wrong number of arguments - expected 3, found " + to_string(argc)).c_str());
+        }
+
+        size_t N, K;
+        set_vec_type const set_bas_vec = calc_set_bas_vec(argv[1], N, K);
+        weig_spec_type const weig_spec = calc_weig_spec(set_bas_vec, N, K);
+        write_weig_spec(argv[2], weig_spec);
+        return EXIT_SUCCESS;
+    } catch (exception const& exc) {
+        cerr << exc.what() << ".\n";
+        return EXIT_FAILURE;
     }
-
-    size_t K = 0;
-    size_t N = 0;
-    set_vec_t set_bas_vec = calc_set_bas_vec(argv[1], K, N);
-    weig_spec_t weig_spec = calc_weig_spec(set_bas_vec, K, N);
-    write_weig_spec(argv[2], weig_spec);
-
-    return EXIT_SUCCESS;
 }
